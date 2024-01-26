167c41d4b55ed7b56e8a386760bc8e7c877af9dc
şöhrett <sohretturaman@gmail.com>
2024-01-26 17:38:32 +0300

    taken data has been sent to server

--- practices3-context/client/src/SoketApi.jsx
+++ practices3-context/client/src/SoketApi.jsx
@@ -13,3 +13,9 @@ export const init = () => {
 		console.log("Sunucuya bağlantı başarıyla gerçekleşti.")
 	);
 };
+
+export const  sendColor =(color)=>{
+  socket.emit('newColor',color); //emit is the keyword to send data fromuser to server , and the verianle name ,s 
+  //which determined in app.js file inside backend folder 
+  //newColor is channel name , and color is my data to send server
+}
--- practices3-context/client/src/components/realtimeserver/Platte.js
+++ practices3-context/client/src/components/realtimeserver/Platte.js
@@ -1,13 +1,29 @@
-import React from "react";
+/** @format */
 
+import React, { useState } from "react";
+import {sendColor} from '../../SoketApi'
 function Platte() {
+  const [color, setColor] = useState('');
+
+
+  const handleColorSoket=()=>{
+    console.log('button clikced and color: ',color);
+    
+    sendColor(color); 
+  }
   return (
     <div className="palette">
-        <input  type="color"  />
-        <br/>
-          <button > Submit Color</button>
+      <input
+        type="color"
+        value={color}
+        onChange={(e) => setColor(e.target.value)}
+      />
+      <br />
+      <button onClick={handleColorSoket}> Submit Color</button>
+      <br/>
+      color : {color}
     </div>
-  )
+  );
 }
 
 export default Platte;
